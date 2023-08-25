627cf7d6eff98df0f93656a04ac3d58dd2381f6f
unknown <patrickkeita80@yahoo.com>
2023-08-25 05:45:48 -0700

    Add controllers, models & dependencies

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+greetings = %w[
+    سلام
+    Hello
+    Bonjour
+    Hola
+    Morning
+  ]
+  
+  greetings.each do |message|
+    Greeting.create!(message: message)
+  end
